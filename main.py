c925b5771069bee1abeaf948ce21dbd5c1a813b2
DevSoleo <devsoleo@protonmail.com>
2022-09-26 16:30:11 +0200

    Correction du bug de décalage de courbe

--- main.py
+++ main.py
@@ -26,14 +26,16 @@ form_dogs = MultipartEncoder(fields={'vote': 'Dogs'})
 form_cats_s = form_cats.to_string()
 form_dogs_s = form_dogs.to_string()
 
-res = []
+first_instance_id = ""
 
-lines = defaultdict(list)
+lines = defaultdict(list) # Contient les données des courbe
 
-instances = Counter()
+instances = Counter() # Contient les données à afficher pour la prochaine seconde (se clear à chaque utilisation)
 instances_l = threading.Lock()
 
-def update_graph(_):
+offset = defaultdict(list) # Contient le décalage de chaque courbes
+
+def update_graph(_): # Chaque seconde
     # Création de la courbe
     plt.cla()
  
@@ -41,12 +43,23 @@ def update_graph(_):
         for k, v in instances.items():
             lines[k].append(v)
         instances.clear()
-
+        
     for instance in lines:
         indexes = list(range(len(lines[instance])))
-        plt.plot(indexes, lines[instance], label = instance, linestyle="-.")
+        
+        print(instance, lines[instance])
+        
+        if offset[instance] == []:
+            offset[instance] = (len(lines[first_instance_id]) - len(lines[instance]))
+
+        shifted_values = [0] * offset[instance] 
+        shifted_values.extend(lines[instance])
+
+        plt.plot(list(range(len(shifted_values))), shifted_values, label = instance, linestyle="-")
 
 def vote():
+    global first_instance_id
+    
     for _ in range(0, 100):
         # Génération d'un vote aléatoire
         i = random.randint(0, 100)
@@ -64,14 +77,14 @@ def vote():
             print("Bad server response", r.status_code)
             continue
         
-        # Lecture de la réponse
         container_id = r.headers['X-HANDLED-BY']
         
-        print(container_id)
+        if first_instance_id == "":
+            first_instance_id = container_id
         
         with instances_l:
             instances[container_id] += 1
-
+       
 def loadtest(exc):
     futures = []
     for _ in range(0, 100):
